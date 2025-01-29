7fa0e8e06e4c4a3c490db2b2fcb6b9decd3cd0d3
Steven Masley <stevenmasley@gmail.com>
2025-01-28 20:58:32 -0600

    test: add unit test vector to show repeated type constraints

--- testdata/union/union.go
+++ testdata/union/union.go
@@ -3,3 +3,9 @@ package union
 type UnionConstraint[T string | int64] struct {
 	Value T
 }
+
+// Repeated constraints are redundant
+// TODO: Write a mutation to remove redundant constraints
+type Repeated[T string | string | int64 | uint64] struct {
+	Value T
+}
