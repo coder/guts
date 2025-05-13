fa2f953a32f1eb4887a19f73214dc8df0d0eac62
Steven Masley <stevenmasley@gmail.com>
2025-05-13 11:51:38 -0500

    fix: alias fields in structs

--- testdata/alias/alias.go
+++ testdata/alias/alias.go
@@ -6,3 +6,12 @@ type AliasNested = Alias
 type Alias = Foo
 type AliasString = string
 type AliasStringSlice = []string
+
+type FooStruct struct {
+	Key string
+}
+
+type AliasStruct = FooStruct
+type AliasStructNested = AliasStruct
+type AliasStructSlice = []FooStruct
+type AliasStructNestedSlice = []AliasStructNested
