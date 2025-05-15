64289704f69ef4b513384fd9f1c06b9e4a69a737
Steven Masley <stevenmasley@gmail.com>
2025-05-15 11:15:28 -0500

    add an extra test case field

--- testdata/nonids/nonids.go
+++ testdata/nonids/nonids.go
@@ -3,6 +3,7 @@ package nonids
 type Foo struct {
 	// Hyphen is an odd case, but this field is not ignored
 	Hyphen     string `json:"-,"`
+	Ignored    string `json:"-"`
 	Hyphenated string `json:"hyphenated-string"`
 	Numbered   int    `json:"1numbered"`
 }
